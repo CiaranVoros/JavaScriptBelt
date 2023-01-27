63099fab48fb17e4d3f621f2373403c7f2d505fe
CiaranVoros <ciaranvoros@gmail.com>
2023-01-27 11:48:49 -0800

    committing

--- server/routes/pet.routes.js
+++ server/routes/pet.routes.js
@@ -1,9 +1,9 @@
 const PetController = require('../controllers/pet.controller')
 
 module.exports = function(app) {
-    app.get('/allPets', PetController.allPets);
-    app.get('/pet/:id', PetController.getPet);
-    app.post('/pet/new', PetController.createPet);
-    app.put('/pet/edit/:id', PetController.updatePet);
-    app.delete('/pet/delete/:id', PetController.deletePet);
+    app.get('/api/allPets', PetController.allPets);
+    app.get('/api/pet/:id', PetController.getPet);
+    app.post('/api/pet/new', PetController.createPet);
+    app.put('/api/pet/edit/:id', PetController.updatePet);
+    app.delete('/api/pet/delete/:id', PetController.deletePet);
 }
